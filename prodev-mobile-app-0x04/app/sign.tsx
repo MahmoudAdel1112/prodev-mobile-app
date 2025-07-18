60eebe3d033b088e703656319be9f67f3876e296
Mahmoud Adel <whowanttolearn@gmail.com>
2025-07-18 12:41:03 +0300

    Add files via upload

---
+++ prodev-mobile-app-0x04/app/sign.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const sign = () => {
+  return (
+    <View>
+      <Text>sign</Text>
+    </View>
+  );
+};
+
+export default sign;
+
+const styles = StyleSheet.create({});
