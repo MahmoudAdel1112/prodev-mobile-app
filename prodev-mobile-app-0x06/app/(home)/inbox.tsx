b3eecb4529e02dee7d5afe35c9fbdc43f170ecd0
Mahmoud Adel <whowanttolearn@gmail.com>
2025-07-18 11:37:50 +0300

    Add files via upload

---
+++ prodev-mobile-app-0x06/app/(home)/inbox.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const inbox = () => {
+  return (
+    <View>
+      <Text>inbox</Text>
+    </View>
+  );
+};
+
+export default inbox;
+
+const styles = StyleSheet.create({});
---
+++ prodev-mobile-app-0x06/app/join.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const join = () => {
+  return (
+    <View>
+      <Text>join</Text>
+    </View>
+  );
+};
+
+export default join;
+
+const styles = StyleSheet.create({});
